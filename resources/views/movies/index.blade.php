c36a68562c5aff7b08a7f6370d073809f6d92e79
SimoneVilardo <simovila95@gmail.com>
2023-08-02 17:33:49 +0200

    completed page index

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,5 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Genere</th>
+                            <th scope="col">Voto</th>
+                            <th scope="col">Azioni</th>                            
+                        </tr>
+                    </thead>
+                    @foreach ($movies as $movie)  
+                        <tbody>
+                            <tr>
+                                <th scope="row">{{ $movie->name }}</th>
+                                <td>{{ $movie->genre }}</td>
+                                <td>{{ $movie->vote }}</td>
+                                <td>
+                                    <a class="btn btn-info btn-sm" href="{{ route('movies.show', $movie->id )}}"><i class="fas fa-eye"></i></a>
+                                    <a class="btn btn-warning btn-sm" href=""><i class="fas fa-pen"></i></a>
+                                    <a class="btn btn-danger btn-sm" href=""><i class="fas fa-trash"></i></a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    @endforeach
+                </table>
+            </div>
+        </div>
+    </div>
 @endsection
