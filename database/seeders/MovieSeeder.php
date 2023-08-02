257b44836cfc71f9f2335e8b6e5b157a99b50c96
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-08-02 16:30:19 +0200

    created seeder-update db

--- database/seeders/MovieSeeder.php
+++ database/seeders/MovieSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Movie;
 
+
 class MovieSeeder extends Seeder
 {
     /**
@@ -15,6 +16,22 @@ class MovieSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $movies= config('movies');
+
+        foreach($movies as $item) {
+            $movie= new Movie();
+
+            $movie->name=$item['name'];
+            $movie->img=$item['img'];
+            $movie->genre=$item['genre'];
+            $movie->vote=$item['vote'];
+            $movie->original_language=$item['original_language'];
+            $movie->length=$item['length'];
+
+
+            $movie->save();
+           
+        }
+
     }
 }
