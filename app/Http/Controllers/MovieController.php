3f68d983da8b27f10230f02c89dc80ee755ee414
SimoneVilardo <simovila95@gmail.com>
2023-08-02 17:06:13 +0200

    complete function index show in MovieController

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -15,7 +15,9 @@ class MovieController extends Controller
      */
     public function index()
     {
-        //
+        $movies = Movie::all();
+
+        return view('movies.index', compact('movies'));
     }
 
     /**
@@ -47,7 +49,7 @@ class MovieController extends Controller
      */
     public function show(Movie $movie)
     {
-        //
+        return view('movies.show', compact('movie'));
     }
 
     /**
